529e1de665d857dff5b00e9c969cb3fe2458b96d
Joey Cho <cjy12182000@naver.com>
2019-01-24 09:46:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,8 @@ class App < Sinatra::Base
   end
   
   post '/puppy' do
-    Puppy.new(params[:name], params[:breed], params)
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
   end
 
 end
